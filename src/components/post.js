2c18eb8d56ca67bc3f4b95ad0a930fd3b2b7602f
jkaunitz <justin.kaunitz@gmail.com>
2018-08-01 16:50:30 -0700

    modified post component to render a result post and setup render links

--- src/components/post.js
+++ src/components/post.js
@@ -9,17 +9,48 @@ class Post extends Component {
         return topics;
     }
 
-    render() {
-        return (
-            <li className="recent-post">
-                <div className="recent-post__title">
-                    {this.props.title}
+    renderLinks() {
+        let links = this.props.post_links.map((post_link, index) => {
+            return (
+                <div className="post-link" key={index}>
+                    <div className="post-link__box"></div>
+                    <div className="post-link__link">
+                        <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+                    </div>
                 </div>
-                <div className="recent-post__topics">
-                    {this.renderTopics()}
-                </div>                    
-            </li>
-        )
+            )
+        })
+        return links;
+    }
+
+    render() {
+        if(this.props.type == 'recent') {
+            return (
+                <li className="recent-post">
+                    <div className="recent-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="recent-post__topics">
+                        {this.renderTopics()}
+                    </div>                    
+                </li>
+            )
+        } else if(this.props.type == 'result') {
+            return (
+                <li className="result-post">
+                    <div className="result-post__topics">
+                        {this.renderTopics()}
+                    </div>
+                    <div className="result-post__title">
+                        {this.props.title}
+                    </div>
+                    <div className="result-post__link">
+                        {this.renderLinks()}
+                    </div>
+                </li>
+            )
+        }
+        
     }
 }
 
